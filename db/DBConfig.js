722717cafe202d9969b1cbeba4bba8ac90ed2409
abdulgrr <131988272+abdulgrr@users.noreply.github.com>
2025-01-22 13:39:10 +0300

    yorum satırı DBConfig.js

--- db/DBConfig.js
+++ db/DBConfig.js
@@ -4,11 +4,11 @@ const sequelize = new Sequelize('bskshr_kds', 'root', '', {
     host: 'localhost',        // Veritabanı sunucusu
     port: 3306,               // MySQL varsayılan portu
     dialect: 'mysql',         // Dialect MySQL olarak ayarlanıyor
-    logging:  console.log,           // SQL sorgularının console'da görünmesini istemiyorsanız false yapın
+    logging:  console.log,           // SQL sorgularının console'da görünmesi
    
 });
 
-// Bağlantıyı test et
+// Bağlantıyı test etme kısmı
 sequelize.authenticate()
     .then(() => {
         console.log('Veritabanı bağlantısı başarılı.');
