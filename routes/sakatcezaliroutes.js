33df7aedcfde8c62cf56cf14301247b4b446e776
abdulgrr <131988272+abdulgrr@users.noreply.github.com>
2025-01-22 13:44:03 +0300

    yorum eklendi sakatcezaliroutes.js

--- routes/sakatcezaliroutes.js
+++ routes/sakatcezaliroutes.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const sakatCezaliController = require('../controller/sakatcezalicontroller');
-
+// form ile veri gönderme
 router.post('/sakatcezali', sakatCezaliController.createSakatCezali);
+// tabloya veri çekme
 router.get('/sakatcezali', sakatCezaliController.getSakatCezali);
+// Güncelleme butonu
 router.put('/sakatcezali/:id', sakatCezaliController.updateSakatCezali);
+// Silme Butomu
 router.delete('/sakatcezali/:id', sakatCezaliController.deleteSakatCezali);
 
 module.exports = router;
